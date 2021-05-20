8d2c2271995bdeb25038ae57250f916a7cfb657e
Chanaka Lasantha <48653609+nwclasantha@users.noreply.github.com>
2021-05-20 14:09:03 +0530

    Add files via upload

---
+++ DiffieHellman.py
@@ -0,0 +1,23 @@
+from KryptoMath import Prime
+
+
+class DH:
+    def __init__(self):
+        self.privatePrime = Prime.rand_prime(2000)
+        self.sharedPrime = Prime.rand_prime(2000)
+        self.base = Prime.rand_prime(2000)
+        self.key = int()
+
+    """
+	Calculate 1. step to the secret
+	"""
+
+    def calcPublicSecret(self):
+        return (self.base ** self.privatePrime) % self.sharedPrime
+
+    """
+	Calculate the shared secret
+	"""
+
+    def calcSharedSecret(self, privSecret):
+        self.key = (privSecret ** self.privatePrime) % self.sharedPrime
